ab67287096b69643993df50c3621e75d30aaf054
Trefex <trefex@gmail.com>
2012-12-14 00:00:25 +0100

    - Converted mg/m3 to ug/m3 which seems to be the accepted standard - Added images from the Blog - Added output for airquality sensor with the Fio
    Signed-off-by: Trefex <trefex@gmail.com>

--- Module_Dust-Sensor/dustSensor.ino
+++ Module_Dust-Sensor/dustSensor.ino
@@ -8,7 +8,8 @@
  For Pin connections, please check the Blog or the github project page
  Authors: Cyrille Médard de Chardon (serialC), Christophe Trefois (Trefex)
  Changelog:
-   2012-Dec
+   2012-Dec-01: Cleaned up code
+   2012-Dec-13: Converted mg/m3 to ug/m3 which seems to be the accepted standard
 
  This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter 
@@ -47,7 +48,7 @@ void loop(){
   
   // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
   // Chris Nafis (c) 2012
-  dustDensity = 0.17 * calcVoltage - 0.1; 
+  dustDensity = (0.17 * calcVoltage - 0.1)*1000; 
   
   Serial.print("Raw Signal Value (0-1023): ");
   Serial.print(voMeasured);
@@ -55,7 +56,7 @@ void loop(){
   Serial.print(" - Voltage: ");
   Serial.print(calcVoltage);
   
-  Serial.print(" - Dust Density: ");
+  Serial.print(" - Dust Density [ug/m3]: ");
   Serial.println(dustDensity);
   
   delay(1000);
