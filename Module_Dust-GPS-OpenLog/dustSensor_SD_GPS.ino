f260779a6f31502a161984305b09e59151ac047c
Trefex <trefex@gmail.com>
2012-12-17 13:09:25 +0100

    - Cleaned up code - Added Barometric Sensor but not used in this code
    Signed-off-by: Trefex <trefex@gmail.com>

--- Module_Dust-GPS-OpenLog/dustSensor_SD_GPS.ino
+++ Module_Dust-GPS-OpenLog/dustSensor_SD_GPS.ino
@@ -16,14 +16,24 @@
  */
  
 #include <SoftwareSerial.h>
-#include <Adafruit_GPS.h>
+#include <Wire.h>
+#include <Arduino.h>
  
 #define measurePin 6
 #define ledPower 12
 
 #define USE_OPENLOG // disable to remove logging
+#define USE_GPS // disable to remove GPS
+//#define USE_BARO // disable to remove Barometric sensor
 #define DEBUG_ON // enable to output debugging information on normal Serial
 #define GPSECHO false // put to true if you want to see raw GPS data flowing
+
+#ifdef USE_BARO
+  #include <Adafruit_BMP085.h>
+#endif
+#ifdef USE_GPS
+  #include <Adafruit_GPS.h>
+#endif 
 
 #define OPENLOG_RST_PIN 6
 #define OPENLOG_TX_PIN 7
@@ -36,11 +46,16 @@
   SoftwareSerial OpenLog(OPENLOG_RX_PIN, OPENLOG_TX_PIN);
 #endif
 
-// Create Soft Serial GPS handle
-SoftwareSerial GPSSerial(ADAGPS_TX_PIN, ADAGPS_RX_PIN);
+#ifdef USE_BARO
+  Adafruit_BMP085 BMP;
+#endif
 
-// Create GPS handle using GPS Soft Serial
-Adafruit_GPS GPS(&GPSSerial);
+#ifdef USE_GPS
+  // Create Soft Serial GPS handle
+  SoftwareSerial GPSSerial(ADAGPS_TX_PIN, ADAGPS_RX_PIN);
+  // Create GPS handle using GPS Soft Serial
+  Adafruit_GPS GPS(&GPSSerial);
+#endif
 
 #ifdef USE_OPENLOG
   //static const int resetOpenLog = OPENLOG_RST_PIN;
@@ -51,7 +66,6 @@ Adafruit_GPS GPS(&GPSSerial);
 // off by default!
 boolean usingInterrupt = false;
 boolean gpsReady = false;
-void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
 
 int samplingTime = 280;
 int deltaTime = 40;
@@ -61,8 +75,13 @@ float voMeasured = 0;
 float calcVoltage = 0;
 float dustDensity = 0;
 
+uint32_t timer = millis();
+
 void setup(){
-  Serial.begin(115200);
+  #ifdef DEBUG_ON
+    Serial.begin(115200);
+  #endif
+  
   pinMode(ledPower,OUTPUT);
   pinMode(ADAGPS_RESET, OUTPUT);
   
@@ -70,54 +89,38 @@ void setup(){
   delay(2000);
   digitalWrite(ADAGPS_RESET, HIGH);
   
-  GPS.begin(9600);
-  delay(40);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  #ifdef USE_GPS
+    GPS.begin(9600);
+    delay(40);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  #endif
   
   #ifdef USE_OPENLOG
     // Setup OpenLog
     OpenLog.begin(9600);
     pinMode(OPENLOG_RST_PIN, OUTPUT);  
   #endif
-
-  // do not use intterupts  
-  useInterrupt(false);
+  
+  #ifdef USE_BARO
+    BMP.begin();
+  #endif
 
   delay(1000);
-  // Ask for firmware version
-  GPSSerial.println(PMTK_Q_RELEASE);
+  #ifdef USE_GPS
+    // Ask for firmware version
+    GPSSerial.println(PMTK_Q_RELEASE);
+  #endif
 }
 
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-    if (c) UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
-}
-
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-    usingInterrupt = true;
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-    usingInterrupt = false;
-  }
-}
-
-uint32_t timer = millis();
 void loop(){
-  Serial.println("Being of loop");
-  if (millis() - timer > 2000) { 
-    
+  delay(2000);
+  #ifdef DEBUG_ON
+    Serial.println("\nBeing of loop");
+  #endif
+
+  // Take measurement every two seconds
+  // if (millis() - timer > 2000) { 
     #ifdef USE_OPENLOG
       OpenLog.listen();
       delay(20);
@@ -143,142 +146,133 @@ void loop(){
     if(dustDensity < 0) {
       dustDensity = 0;
     }
-    if(GPS.fix) {
-      #ifdef USE_OPENLOG
-        OpenLog.print("<< ");
-        OpenLog.print(dustDensity);
-        OpenLog.print(";");
-        OpenLog.print(GPS.latitude, 4); OpenLog.print(GPS.lat);
-        OpenLog.print(";"); 
-        OpenLog.print(GPS.longitude, 4); OpenLog.println(GPS.lon);
-        delay(15);
-      #endif
-    }
     
-    #ifdef DEBUG_ON 
-      Serial.print("\nRaw Signal Value (0-1023): ");  Serial.print(voMeasured);
-      Serial.print(" - Voltage: ");  Serial.print(calcVoltage);
-      Serial.print(" - Dust Density [ug/m3]: ");  Serial.println(dustDensity);
-    #endif
-  }
-  
-  GPSSerial.listen();
-  //delay(1000);
+    // Begin new line
+    #ifdef USE_OPENLOG
 
-  unsigned long start = millis(); // begin listening to the GPS
-  while (start + 2000 > millis()) {
-   
-  if (! usingInterrupt) {
-      // read data from the GPS in the 'main loop'
-      char c = GPS.read();
-      // if you want to debug, this is a good time to do it!
-      if (GPSECHO)
-        if (c) UDR0 = c;
-        // writing direct to UDR0 is much much faster than Serial.print 
-        // but only one character can be written at a time. 
-  }
+    #endif
     
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences! 
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-   // Serial.println("New GPS Sentence received");
-    //gpsReady = true;
-    //break;
-    if (GPS.parse(GPS.lastNMEA())){   // this also sets the newNMEAreceived() flag to false
-     // return;  // we can fail to parse a sentence in which case we should just wait for another
-      
-      if (GPS.fix) {
-        #ifdef DEBUG_ON 
-          Serial.print("\nTime: ");
-          Serial.print(GPS.hour, DEC); Serial.print(':');
-          Serial.print(GPS.minute, DEC); Serial.print(':');
-          Serial.print(GPS.seconds, DEC); Serial.print('.');
-          Serial.println(GPS.milliseconds);
-          Serial.print("Date: ");
-          Serial.print(GPS.day, DEC); Serial.print('/');
-          Serial.print(GPS.month, DEC); Serial.print("/20");
-          Serial.println(GPS.year, DEC);
-          Serial.print("Fix: "); Serial.print((int)GPS.fix);
-          Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
-          Serial.print("Location: ");
-          Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-          Serial.print(", "); 
-          Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-          
-          Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-          Serial.print("Angle: "); Serial.println(GPS.angle);
-          Serial.print("Altitude: "); Serial.println(GPS.altitude);
-          Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    #ifdef USE_GPS // If GPS is on, print all sensor values, else only Dust
+      if(GPS.fix) {
+        #ifdef USE_OPENLOG
+          OpenLog.print("<< ");
+          OpenLog.print(GPS.day, DEC); OpenLog.print("/");
+          OpenLog.print(GPS.month, DEC); OpenLog.print("/20");
+          OpenLog.print(GPS.year, DEC); OpenLog.print(";");
+          OpenLog.print(GPS.hour, DEC); OpenLog.print(":");
+          delay(15);
+          OpenLog.print(GPS.minute, DEC); OpenLog.print(":");
+          OpenLog.print(GPS.seconds, DEC); OpenLog.print(";");
+          OpenLog.print(GPS.latitude, 4); OpenLog.print(GPS.lat); OpenLog.print(";"); 
+          OpenLog.print(GPS.longitude, 4); OpenLog.print(GPS.lon); OpenLog.print(";");
+          // Dust Values
+          OpenLog.print(dustDensity); OpenLog.print(";");
+          delay(15);
+          #ifdef USE_BARO
+            OpenLog.print(BMP.readTemperature()); OpenLog.print(";");
+            OpenLog.println(BMP.readPressure()); 
+          #endif
+          delay(15);
         #endif
       }
-    }
-    }
-  }
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
-
-  // approximately every 2 seconds or so, print out the current stats
-  if ((millis() - timer > 2000) && gpsReady) { 
-
+    #else
+      #ifdef USE_OPENLOG
+        OpenLog.print("<< ");
+        OpenLog.print(dustDensity); OpenLog.print(";");
+      #endif
+    #endif 
     
-    Serial.print("\nRaw Signal Value (0-1023): ");
-    Serial.print(voMeasured);
-    
-    Serial.print(" - Voltage: ");
-    Serial.print(calcVoltage);
-    
-    Serial.print(" - Dust Density [ug/m3]: ");
-    Serial.println(dustDensity);
-    
-    timer = millis(); // reset the timer
-    
-    #ifdef USE_OPENLOG
-      OpenLog.print(";");
-      OpenLog.print(GPS.hour, DEC); OpenLog.print(':');
-      OpenLog.print(GPS.minute, DEC); OpenLog.print(':');
-      OpenLog.print(GPS.seconds, DEC); OpenLog.print('.');
-      OpenLog.print(GPS.milliseconds);
-      OpenLog.print(";");
-      OpenLog.print(GPS.day, DEC); OpenLog.print('/');
-      OpenLog.print(GPS.month, DEC); OpenLog.print("/20");
-      OpenLog.println(GPS.year, DEC);
-      delay(15);
+    #ifdef USE_BARO
+      #ifdef DEBUG_ON
+        Serial.print("\nTemperature: "); Serial.print(BMP.readTemperature()); Serial.println(" *C");
+        Serial.print("Pressure: "); Serial.print(BMP.readPressure()); Serial.println(" Pa");
+        Serial.print("Altitude: "); Serial.print(BMP.readAltitude()); Serial.println(" meters");
+      #endif  
+      #ifndef USE_GPS // Print BMP alone when no GPS is present
+        #ifdef USE_OPENLOG
+          OpenLog.print(BMP.readTemperature()); OpenLog.print(";");
+          OpenLog.println(BMP.readPressure()); 
+          delay(15);
+        #endif
+      #endif
     #endif
     
-    Serial.print("\nTime: ");
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
-    if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", "); 
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+    #ifdef DEBUG_ON 
+      Serial.print("Dust Density [ug/m3]: ");  Serial.println(dustDensity);
+    #endif
+ // }
+  
+  #ifdef USE_GPS
+    GPSSerial.listen();
+    //delay(1000);
+  #endif
+
+  unsigned long start = millis(); // begin listening to the GPS
+  // Listen to GPS for two seconds or until fix has been reached, whichever comes first
+  #ifdef USE_GPS
+    while (start + 2000 > millis()) {
+      if (! usingInterrupt) {
+          // read data from the GPS in the 'main loop'
+          char c = GPS.read();
+          // if you want to debug, this is a good time to do it!
+          if (GPSECHO)
+            if (c) UDR0 = c;
+            // writing direct to UDR0 is much much faster than Serial.print 
+            // but only one character can be written at a time. 
+      }
       
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      // if a sentence is received, we can check the checksum, parse it...
+      if (GPS.newNMEAreceived()) {
+        // a tricky thing here is if we print the NMEA sentence, or data
+        // we end up not listening and catching other sentences! 
+        // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+        //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+        // Serial.println("New GPS Sentence received");
+        //gpsReady = true;
+        //break;
+        if (GPS.parse(GPS.lastNMEA())){   // this also sets the newNMEAreceived() flag to false
+          if (GPS.fix) {
+            #ifdef DEBUG_ON 
+                Serial.print("\nTime: ");
+                Serial.print(GPS.hour, DEC); Serial.print(':');
+                Serial.print(GPS.minute, DEC); Serial.print(':');
+                Serial.print(GPS.seconds, DEC); Serial.print('.');
+                Serial.println(GPS.milliseconds);
+                Serial.print("Date: ");
+                Serial.print(GPS.day, DEC); Serial.print('/');
+                Serial.print(GPS.month, DEC); Serial.print("/20");
+                Serial.println(GPS.year, DEC);
+                Serial.print("Fix: "); Serial.print((int)GPS.fix);
+                Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+                Serial.print("Location: ");
+                Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+                Serial.print(", "); 
+                Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+                
+                Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+                Serial.print("Angle: "); Serial.println(GPS.angle);
+                Serial.print("Altitude: "); Serial.println(GPS.altitude);
+                Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+                break;
+            #endif
+          }
+        }
+      }
     }
-    gpsReady = false;
-  }
+  #endif
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
   //clearSerial();
   //delay(400);
 }
 
 void clearSerial() {
-  while (Serial.read() >= 0) {;}
-  while(GPSSerial.read() >= 0) {;}
+  #ifdef DEBUG_ON
+    while (Serial.read() >= 0) {;}
+  #endif
+  #ifdef USE_GPS
+    while(GPSSerial.read() >= 0) {;}
+  #endif
   //while(OpenLog.read() >= 0){;}
 }
 
