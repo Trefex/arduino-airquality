6325ce74437f518763920388eef285c5bdb48b99
cyrille <cyrillem@gmail.com>
2013-03-16 20:44:06 +0100

    Created the new folder holding the 5 sensors and libraries all together Code compiles successfully!

---
+++ aq5s/SHT1x.cpp
@@ -0,0 +1,242 @@
+/**
+ * SHT1x Library
+ *
+ * Modified by Cyrille Medard de Chardon for 3V3 Arduino Fio
+ *
+ * Copyright 2009 Jonathan Oxer <jon@oxer.com.au> / <www.practicalarduino.com>
+ * Based on previous work by:
+ *    Maurice Ribble: <www.glacialwanderer.com/hobbyrobotics/?p=5>
+ *    Wayne ?: <ragingreality.blogspot.com/2008/01/ardunio-and-sht15.html>
+ *
+ * Manages communication with SHT1x series (SHT10, SHT11, SHT15)
+ * temperature / humidity sensors from Sensirion (www.sensirion.com).
+ */
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include "SHT1x.h"
+
+SHT1x::SHT1x(int dataPin, int clockPin)
+{
+  _dataPin = dataPin;
+  _clockPin = clockPin;
+  _temperatureC = -273.0;
+}
+
+/* ================  Public methods ================ */
+
+/**
+ * To be called after readHumidity() which calls readTemperatureC()
+ */
+
+float SHT1x::retrieveTemperatureC()
+{
+  return _temperatureC;
+}
+
+/**
+ * Reads the current temperature in degrees Celsius
+ */
+float SHT1x::readTemperatureC()
+{
+  int _val;                // Raw value returned from sensor
+  float _temperature;      // Temperature derived from raw value
+
+  // Conversion coefficients from SHT1x V5 datasheet
+  const float D1 = -39.66; // for 14 bit @ 3V3
+  const float D2 =   0.01; // for 14 Bit DEGC
+
+  // Fetch raw value
+  _val = readTemperatureRaw();
+
+  // Convert raw value to degrees Celsius
+  _temperature = (_val * D2) + D1;
+
+  // Set global variable
+  _temperatureC = _temperature;
+
+  //return (_val);
+  return (_temperature);
+}
+
+/**
+ * Reads current temperature-corrected relative humidity
+ */
+float SHT1x::readHumidity()
+{
+  int _val;                    // Raw humidity value returned from sensor
+  float _linearHumidity;       // Humidity with linear correction applied
+  float _correctedHumidity;    // Temperature-corrected humidity
+  float _temperature;          // Raw temperature value
+
+  // Conversion coefficients from SHT1x V5 datasheet
+//  const float C1 = -4.0;       // for 12 Bit
+//  const float C2 =  0.0405;    // for 12 Bit
+//  const float C3 = -0.0000028; // for 12 Bit
+  const float C1 = -2.0468;	// for 12 bit
+  const float C2 = 0.0367;	// for 12 bit
+  const float C3 = -0.0000015955;// for 12 bit, see Table 6 in SHT1x V5 datasheet
+  const float T1 =  0.01;      // for 14 Bit - independent of voltage
+  const float T2 =  0.00008;   // for 14 Bit - independent of voltage
+
+  // Command to send to the SHT1x to request humidity
+  int _gHumidCmd = 0b00000101;
+
+  // Fetch the value from the sensor
+  sendCommandSHT(_gHumidCmd, _dataPin, _clockPin);
+  waitForResultSHT(_dataPin);
+  _val = getData16SHT(_dataPin, _clockPin);
+
+  // Apply linear conversion to raw value
+  _linearHumidity = C1 + C2 * _val + C3 * _val * _val;
+
+  // Get current temperature for humidity correction
+  _temperature = readTemperatureC();
+
+  // Correct humidity value for current temperature
+  _correctedHumidity = (_temperature - 25.0 ) * (T1 + T2 * _val) + _linearHumidity;
+
+  return (_correctedHumidity);
+}
+
+
+/* ================  Private methods ================ */
+
+/**
+ * Reads the current raw temperature value
+ */
+float SHT1x::readTemperatureRaw()
+{
+  int _val;
+
+  // Command to send to the SHT1x to request Temperature
+  int _gTempCmd  = 0b00000011;
+
+  sendCommandSHT(_gTempCmd, _dataPin, _clockPin);
+  waitForResultSHT(_dataPin);
+  _val = getData16SHT(_dataPin, _clockPin);
+  //skipCrcSHT(_dataPin, _clockPin);
+
+  return (_val);
+}
+
+/**
+ */
+int SHT1x::shiftIn(int _dataPin, int _clockPin, int _numBits)
+{
+  int ret = 0;
+  int i;
+
+  for (i=0; i<_numBits; ++i)
+  {
+     digitalWrite(_clockPin, HIGH);
+     delay(10);  // I don't know why I need this, but without it I don't get my 8 lsb of temp
+     ret = ret*2 + digitalRead(_dataPin);
+     digitalWrite(_clockPin, LOW);
+  }
+
+  return(ret);
+}
+
+/**
+ */
+void SHT1x::sendCommandSHT(int _command, int _dataPin, int _clockPin)
+{
+  int ack;
+
+  // Transmission Start
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, LOW);
+  digitalWrite(_clockPin, LOW);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  // The command (3 msb are addresses and must be 000, and last 5 bits are command)
+  // MSB = Most Significant Bit, Sends _command one bit at a time from left to right
+  shiftOut(_dataPin, _clockPin, MSBFIRST, _command);
+
+  // Verify we get the correct ack
+  digitalWrite(_clockPin, HIGH);
+  pinMode(_dataPin, INPUT);
+  ack = digitalRead(_dataPin);
+  if (ack != LOW) {
+    Serial.println("Ack Error 0");
+  }
+  digitalWrite(_clockPin, LOW);
+  ack = digitalRead(_dataPin);
+  if (ack != HIGH) {
+    Serial.println("Ack Error 1");
+  }
+}
+
+/**
+ */
+void SHT1x::waitForResultSHT(int _dataPin)
+{
+  int i;
+  int ack;
+
+  pinMode(_dataPin, INPUT);
+
+  for(i= 0; i < 100; ++i)
+  {
+    delay(10);
+    ack = digitalRead(_dataPin);
+
+    if (ack == LOW) {
+      break;
+    }
+  }
+
+  if (ack == HIGH) {
+    Serial.println("Ack Error 2");
+  }
+}
+
+/**
+ */
+int SHT1x::getData16SHT(int _dataPin, int _clockPin)
+{
+  int val;
+
+  // Get the 8 left/most significant bits
+  pinMode(_dataPin, INPUT);
+  pinMode(_clockPin, OUTPUT);
+  val = shiftIn(_dataPin, _clockPin, 8);
+  // ~25 degrees celcius == 25
+  val <<= 8;  //shift 8 bits left
+  // ~25 degrees celsius == 6400
+
+  // Send the required DATA ACK, set to LOW
+  pinMode(_dataPin, OUTPUT);
+  digitalWrite(_dataPin, LOW);
+
+  // Send SCK ACK
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  // Get the least significant bits
+  pinMode(_dataPin, INPUT);
+  // bit-wise OR, like adding
+  val |= shiftIn(_dataPin, _clockPin, 8);
+
+  // I don't understand why this is helping/necessary
+  // tempermental! It's fine without now?
+  // Cable length?
+  //delay(1);
+
+  // We are not performing CRC so don't drop DATA to LOW
+  // ACK receipt of LSb
+  // pinMode of SCK already set to OUTPUT
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  return val;
+}
---
+++ aq5s/aq5s.ino
@@ -0,0 +1,310 @@
+/*
+ Module using an Arduino Fio, a Sharp Optical Dust Sensor GP2Y1010AU0F,
+ an Adafruit Ultimate GPS Breakout v3 and an OpenLog data logger.
+ 
+ Blog: http://arduinodev.woofex.net/2012/12/16/sharp-dust-sensor-with-adafruit-gps-v3/
+ Code: https://github.com/Trefex/arduino-airquality/
+
+ For Pin connections, please check the Blog or the github project page
+ Authors: Cyrille Médard de Chardon (serialC), Christophe Trefois (Trefex)
+ Changelog:
+   2012-Dec-16: First version working. Need to clean-up code
+
+ This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter 
+ to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. 
+ */
+ 
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Arduino.h>
+
+#define USE_OPENLOG // disable to remove logging
+#define USE_GPS // disable to remove GPS
+#define USE_BARO // disable to remove Barometric sensor
+#define DEBUG_ON // enable to output debugging information on normal Serial
+#define GPSECHO true // put to true if you want to see raw GPS data flowing
+#define USE_HT // comment to remove Humidity and temperature sensor
+
+// Check which sensors are being used based on defines above
+#ifdef USE_BARO
+  #include "Adafruit_BMP085.h"
+#endif
+
+#ifdef USE_GPS
+  #include "Adafruit_GPS.h"
+#endif
+
+#ifdef USE_HT
+  #include "SHT1x.h"
+#endif
+
+// Pin definitions
+#define HT_DATA_PIN 10  // Humidity/Temp sensor data
+#define HT_SCK_PIN 11  // Humidity/Temp sensor serial clock
+
+#define DUST_RX_PIN 6  // Dust sensor (Analog 6)
+#define DUST_LED_PIN 12  // Dust sensor led
+
+#define OPENLOG_RST_PIN 6  // Openlogger (Digital 6)
+#define OPENLOG_TX_PIN 7  // Openlogger
+#define OPENLOG_RX_PIN 8  // Openlogger
+
+#define ADAGPS_RX_PIN 2 // GPS RX to Pin ADAGPS_RX_PIN
+#define ADAGPS_TX_PIN 3 // GPS TX to ADAGPS_TX_PIN
+#define ADAGPS_RESET 10 // Pull Low to switch off the module
+
+// Constructor declarations
+#ifdef USE_HT
+  SHT1x th_sensor(HT_DATA_PIN, HT_SCK_PIN);
+#endif
+
+#ifdef USE_OPENLOG
+  SoftwareSerial OpenLog(OPENLOG_RX_PIN, OPENLOG_TX_PIN);
+#endif
+
+#ifdef USE_BARO
+  Adafruit_BMP085 BMP;
+#endif
+
+#ifdef USE_GPS
+  // Create Soft Serial GPS handle
+  SoftwareSerial GPSSerial(ADAGPS_TX_PIN, ADAGPS_RX_PIN);
+  // Create GPS handle using GPS Soft Serial
+  Adafruit_GPS GPS(&GPSSerial);
+#endif
+
+// variable initializations
+#ifdef USE_OPENLOG
+  //static const int resetOpenLog = OPENLOG_RST_PIN;
+  boolean openlog_ready = false;
+#endif
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+boolean gpsReady = false;
+
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+uint32_t timer = millis();
+
+void setup(){
+  #ifdef DEBUG_ON
+    Serial.begin(115200);
+  #endif
+  
+  pinMode(DUST_LED_PIN,OUTPUT);
+  pinMode(ADAGPS_RESET, OUTPUT);
+  
+  digitalWrite(ADAGPS_RESET, LOW);
+  delay(2000);
+  digitalWrite(ADAGPS_RESET, HIGH);
+  
+  #ifdef USE_GPS
+    GPS.begin(9600);
+    delay(40);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  #endif
+  
+  #ifdef USE_OPENLOG
+    // Setup OpenLog
+    OpenLog.begin(9600);
+    pinMode(OPENLOG_RST_PIN, OUTPUT);  
+  #endif
+  
+  #ifdef USE_BARO
+    BMP.begin();
+  #endif
+
+  delay(1000);
+  #ifdef USE_GPS
+    // Ask for firmware version
+    GPSSerial.println(PMTK_Q_RELEASE);
+  #endif
+}
+
+void loop(){
+  delay(2000);
+  #ifdef DEBUG_ON
+    Serial.println("\nBeing of loop");
+  #endif
+
+  // Take measurement every two seconds
+  // if (millis() - timer > 2000) { 
+    #ifdef USE_OPENLOG
+      OpenLog.listen();
+      delay(20);
+    #endif
+ 
+    #ifdef USE_HT 
+      float temp_c;
+      float humid;
+    #endif
+ 
+    digitalWrite(DUST_LED_PIN,LOW); // power on the LED
+    delayMicroseconds(samplingTime);
+  
+    voMeasured = analogRead(DUST_RX_PIN); // read the dust value
+    
+    delayMicroseconds(deltaTime);
+    digitalWrite(DUST_LED_PIN,HIGH); // turn the LED off
+    delayMicroseconds(sleepTime);
+  
+    // 0 - 3.3V mapped to 0 - 1023 integer values
+    // recover voltage
+    calcVoltage = voMeasured * (3.3 / 1024); 
+    
+    // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+    // Chris Nafis (c) 2012
+    dustDensity = (0.17 * calcVoltage - 0.1)*1000; 
+    
+    //if(dustDensity < 0) {
+      //dustDensity = 0;
+    //}
+
+    // Get temperature
+    #ifdef USE_HT
+      // Read values from the sensor
+      humid = th_sensor.readHumidity();
+      temp_c = th_sensor.retrieveTemperatureC();
+    #endif
+    
+    #ifdef USE_GPS // If GPS is on, print all sensor values, else only Dust
+      if(GPS.fix) {
+        #ifdef USE_OPENLOG
+          OpenLog.print("<< ");
+          OpenLog.print(GPS.day, DEC); OpenLog.print("/");
+          OpenLog.print(GPS.month, DEC); OpenLog.print("/20");
+          OpenLog.print(GPS.year, DEC); OpenLog.print(";");
+          OpenLog.print(GPS.hour, DEC); OpenLog.print(":");
+          delay(15);
+          OpenLog.print(GPS.minute, DEC); OpenLog.print(":");
+          OpenLog.print(GPS.seconds, DEC); OpenLog.print(";");
+          OpenLog.print(GPS.latitude, 4); OpenLog.print(GPS.lat); OpenLog.print(";"); 
+          OpenLog.print(GPS.longitude, 4); OpenLog.print(GPS.lon); OpenLog.print(";");
+          // Dust Values
+          OpenLog.print(dustDensity); OpenLog.print(";");
+          delay(15);
+          #ifdef USE_BARO
+            OpenLog.print(BMP.readTemperature()); OpenLog.print(";");
+            OpenLog.println(BMP.readPressure()); 
+          #endif
+          
+          #ifdef USE_HT
+            OpenLog.print(temp_c);
+            OpenLog.println(humid);
+          #endif
+          
+          delay(15);
+        #endif
+      }
+    #else
+      #ifdef USE_OPENLOG
+        OpenLog.print("<< ");
+        OpenLog.print(dustDensity); OpenLog.print(";");
+      #endif
+    #endif 
+    
+    #ifdef USE_BARO
+      #ifdef DEBUG_ON
+        Serial.print("\nTemperature: "); Serial.print(BMP.readTemperature()); Serial.println(" *C");
+        Serial.print("Pressure: "); Serial.print(BMP.readPressure()); Serial.println(" Pa");
+        Serial.print("Altitude: "); Serial.print(BMP.readAltitude()); Serial.println(" meters");
+      #endif  
+      #ifndef USE_GPS // Print BMP alone when no GPS is present
+        #ifdef USE_OPENLOG
+          OpenLog.print(BMP.readTemperature()); OpenLog.print(";");
+          OpenLog.println(BMP.readPressure()); 
+          delay(15);
+        #endif
+      #endif
+    #endif
+    
+    #ifdef DEBUG_ON 
+      Serial.print("Dust Density [ug/m3]: ");  Serial.println(dustDensity);
+    #endif
+ // }
+  
+  #ifdef USE_GPS
+    GPSSerial.listen();
+    //delay(1000);
+  #endif
+
+  unsigned long start = millis(); // begin listening to the GPS
+  // Listen to GPS for two seconds or until fix has been reached, whichever comes first
+  #ifdef USE_GPS
+    while (start + 2000 > millis()) {
+      if (! usingInterrupt) {
+          // read data from the GPS in the 'main loop'
+          char c = GPS.read();
+          // if you want to debug, this is a good time to do it!
+          if (GPSECHO)
+            if (c) UDR0 = c;
+            // writing direct to UDR0 is much much faster than Serial.print 
+            // but only one character can be written at a time. 
+      }
+      
+      // if a sentence is received, we can check the checksum, parse it...
+      if (GPS.newNMEAreceived()) {
+        // a tricky thing here is if we print the NMEA sentence, or data
+        // we end up not listening and catching other sentences! 
+        // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+        Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+        Serial.println("New GPS Sentence received");
+        //gpsReady = true;
+        //break;
+        if (GPS.parse(GPS.lastNMEA())){   // this also sets the newNMEAreceived() flag to false
+          if (GPS.fix) {
+            #ifdef DEBUG_ON 
+                Serial.print("\nTime: ");
+                Serial.print(GPS.hour, DEC); Serial.print(':');
+                Serial.print(GPS.minute, DEC); Serial.print(':');
+                Serial.print(GPS.seconds, DEC); Serial.print('.');
+                Serial.println(GPS.milliseconds);
+                Serial.print("Date: ");
+                Serial.print(GPS.day, DEC); Serial.print('/');
+                Serial.print(GPS.month, DEC); Serial.print("/20");
+                Serial.println(GPS.year, DEC);
+                Serial.print("Fix: "); Serial.print((int)GPS.fix);
+                Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+                Serial.print("Location: ");
+                Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+                Serial.print(", "); 
+                Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+                
+                Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+                Serial.print("Angle: "); Serial.println(GPS.angle);
+                Serial.print("Altitude: "); Serial.println(GPS.altitude);
+                Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+                break;
+            #endif
+          }
+        }
+      }
+    }
+  #endif
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+  //clearSerial();
+  //delay(400);
+}
+
+void clearSerial() {
+  #ifdef DEBUG_ON
+    while (Serial.read() >= 0) {;}
+  #endif
+  #ifdef USE_GPS
+    while(GPSSerial.read() >= 0) {;}
+  #endif
+  //while(OpenLog.read() >= 0){;}
+}
+
+
