a7a29c01a85f32d1c3a241da59ce3916b9ad1254
cyrille <cyrillem@gmail.com>
2013-11-30 00:38:51 +0100

    Corrected code but NPN connection not working correctly - always on

--- Module_COSensor/C0_test/C0_test.ino
+++ Module_COSensor/C0_test/C0_test.ino
@@ -2,20 +2,19 @@
   #############################################
   C0 Sensor (MQ-7 on Parallax Gas Sensor Module
   basic test using Arduino Fio (3.3V) with additional
-  battery source
+  battery/power source
   #############################################
   
-  When heating coil, the arduino fio LED is off, and on during coil cooling.
-  See output for readings showing if heating is on and read value
+  When heating the coil
  */
 
 // Pin definitions
-#define ACTIVE_MONOX_PIN 13
+#define ACTIVE_MONOX_PIN 11
 #define READ_MONOX_PIN  7
 
 // literal/constants definitions
-#define HIGH_V_TIME 60
-#define LOW_V_TIME 90
+#define HIGH_V_TIME 6 //60
+#define LOW_V_TIME 9 //90
 
 // Variable initializations
 int reading; // holds value [0 - 1023]
@@ -32,29 +31,31 @@ void loop() {
   
   int x;
   
-  // NOTE: HSW input is active-low
-  
-  // turn off sensor and let it cool
-  digitalWrite(ACTIVE_MONOX_PIN, HIGH);
-  Serial.println("Cool readings:");
-  for( x = 0; x < LOW_V_TIME; x++) {
-    delay(1000);
-    Serial.println(analogRead(READ_MONOX_PIN));
-  }
-  
-  // turn on sensor to 'clean' it/remove humidity, wait
-  digitalWrite(ACTIVE_MONOX_PIN, LOW);
-  Serial.println("\nHot readings:");
+  // turn on heating coil to 'clean' it/remove humidity, wait
+  pinMode(ACTIVE_MONOX_PIN, OUTPUT);
+  analogWrite(ACTIVE_MONOX_PIN, 128);
+  Serial.println("\nHot/purge readings:");
   for( x = 0; x < HIGH_V_TIME; x++) {
     delay(1000);
     Serial.println(analogRead(READ_MONOX_PIN));
   }
   
-  // now measure, before we turn it off again
+  // turn off sensor and let it cool
+  analogWrite(ACTIVE_MONOX_PIN, 0);
+  pinMode(ACTIVE_MONOX_PIN, INPUT);
+  Serial.println("Cool/sense readings:");
+  for( x = 0; x < LOW_V_TIME; x++) {
+    delay(1000);
+    Serial.println(analogRead(READ_MONOX_PIN));
+  }
+  
+  // now measure, before we turn it the heating coil on again
   reading = analogRead(READ_MONOX_PIN);
   Serial.print("\nRecorded reading: ");
   Serial.println(reading);
 }
+
+
 
 
 
