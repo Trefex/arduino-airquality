76fad4925633e48a33c9eaac3296959dd333336f
cyrille <cyrillem@gmail.com>
2013-12-23 22:30:51 +0100

    added voltage monitoring to circuit

--- Module_COSensor/C0_test/C0_test.ino
+++ Module_COSensor/C0_test/C0_test.ino
@@ -19,7 +19,8 @@
 // Pin definitions
 #define ACTIVE_MONOX_PIN 11
 #define ACTIVE_MONOX_LED_PIN 13
-#define READ_MONOX_PIN  7
+#define READ_MONOX_PIN A7
+#define READ_COPSV_PIN A1
 
 // literal/constants definitions
 #define HIGH_V_TIME 60 //60
@@ -27,61 +28,117 @@
 
 // Variable initializations
 int reading; // holds value [0 - 1023]
+char mode, next_mode;
 
 void setup() {                
   // initialize pins' modes
   pinMode(ACTIVE_MONOX_PIN, OUTPUT);     
   pinMode(ACTIVE_MONOX_LED_PIN, OUTPUT);     
   pinMode(READ_MONOX_PIN, INPUT);
+  pinMode(READ_COPSV_PIN, INPUT);
+  
+  next_mode = 'I';
   
   Serial.begin(9600);
 }
 
 void loop() {
   
-  int x;
+  int x, duration, psv_reading;
   
-  // turn on heating coil to 'clean' it/remove humidity, wait
-  pinMode(ACTIVE_MONOX_PIN, OUTPUT);
-  analogWrite(ACTIVE_MONOX_PIN, 255);
-  // turn on led to indicate we are heating
-  digitalWrite(ACTIVE_MONOX_LED_PIN, HIGH);
+  // determine what needs to happen this loop
+  mode = next_mode;
   
-  for( x = 0; x < HIGH_V_TIME; x++) {
-    Serial.print("H,");
-    Serial.println(analogRead(READ_MONOX_PIN));
-    delay(1000);
+  switch( mode ) {
+    // Initialize
+    case 'I':
+      // turn on heating coil to 'clean' it/remove humidity, wait
+      pinMode(ACTIVE_MONOX_PIN, OUTPUT);
+      analogWrite(ACTIVE_MONOX_PIN, 255);
+      // turn on led to indicate we are heating
+      digitalWrite(ACTIVE_MONOX_LED_PIN, HIGH);
+      
+      // burn for 500 seconds for initialization
+      duration = 500;
+      
+      next_mode = 'C';
+      break;
+      
+    // Heat
+    case 'H':
+      // turn on heating coil to 'clean' it/remove humidity, wait
+      pinMode(ACTIVE_MONOX_PIN, OUTPUT);
+      analogWrite(ACTIVE_MONOX_PIN, 255);
+      // turn on led to indicate we are heating
+      digitalWrite(ACTIVE_MONOX_LED_PIN, HIGH);
+      
+      duration = HIGH_V_TIME;
+      next_mode = 'C';
+      break;
+      
+    // Cool
+    case 'C':
+      // turn off led indicating we are heating
+      digitalWrite(ACTIVE_MONOX_LED_PIN, LOW);
+      
+      // Turn down heater from 5V to 1.4V for 'cooling'
+      // We need to allow 1.4 V through the heating coil, down from 5V
+      // We can consider putting 1.4V as putting 5V only 1.4/5 of the time
+      // Because we are using PWM to open a transistor, we can allow current to flow from collector
+      // to emitter for 1.4/5 of the time, this means a PWM value (which ranges from 0-255) of 1.4/5*255 = 71.4
+      // We are in fact allowing the 5V go go through 71 times for 1/255 of a second - this creates an avg of 1.4V
+      pinMode(ACTIVE_MONOX_PIN, INPUT);
+      analogWrite(ACTIVE_MONOX_PIN, 71);
+      
+      duration = LOW_V_TIME;
+      next_mode = 'R';
+      break;
+      
+    // Read
+    case 'R':
+      // now measure
+      // turn thw PWM pin to 0 so that we don't measure while in the middle of a heating pulse
+      analogWrite(ACTIVE_MONOX_PIN, 0);
+      reading = analogRead(READ_MONOX_PIN);
+      Serial.print(mode);
+      Serial.print(",");
+      Serial.print(reading);
+      Serial.print(",");
+      Serial.println(analogRead(READ_COPSV_PIN));
+      
+      next_mode = 'H';
+      return;
+      //break;
+      
+    // Shutdown
+    case 'S':
+      analogWrite(ACTIVE_MONOX_PIN, 0);
+      
+      // stay in this mode indefinitely
+      //mode = 'S';
+      duration = 60;
+      break;
+      
   }
 
-  // turn off led indicating we are heating
-  digitalWrite(ACTIVE_MONOX_LED_PIN, LOW);
-  
-  // Turn down heater from 5V to 1.4V for 'cooling'
-  // We need to allow 1.4 V through the heating coil, down from 5V
-  // We can consider putting 1.4V as putting 5V only 1.4/5 of the time
-  // Because we are using PWM to open a transistor, we can allow current to flow from collector
-  // to emitter for 1.4/5 of the time, this means a PWM value (which ranges from 0-255) of 1.4/5*255 = 71.4
-  // We are in fact allowing the 5V go go through 71 times for 1/255 of a second - this creates an avg of 1.4V
-  pinMode(ACTIVE_MONOX_PIN, INPUT);
-  analogWrite(ACTIVE_MONOX_PIN, 71);
-  
-  for( x = 0; x < LOW_V_TIME; x++) {
-    Serial.print("C,");
-    Serial.println(analogRead(READ_MONOX_PIN));
+  // loop to print out data    
+  for( x = 0; x < duration; x++) {
+    Serial.print(mode);
+    Serial.print(",");
+    Serial.print(analogRead(READ_MONOX_PIN));
+    Serial.print(",");
+    psv_reading = analogRead(READ_COPSV_PIN);
+    Serial.println(psv_reading);
+    
+    // check the reading value
+    if( mode != 'I' && (psv_reading < 400 || psv_reading > 800 )) {
+      next_mode = 'S';
+      Serial.println("#CO voltage is irregular, shutting down sensor heating!");
+      return;
+    }
     delay(1000);
   }
-  
-  // now measure
-  // turn thw PWM pin to 0 so that we don't measure while in the middle of a heating pulse
-  analogWrite(ACTIVE_MONOX_PIN, 0);
-  
-  reading = analogRead(READ_MONOX_PIN);
-  Serial.print("R,");
-  Serial.println(reading);
-  
-  // restart loop and turn the heating coil back on again
 }
-
 
 
 
--- Module_COSensor/outputs/analysis.R
+++ Module_COSensor/outputs/analysis.R
@@ -4,10 +4,17 @@ plot_co_data('output2.txt')
 plot_co_data('output3.txt')
 plot_co_data('output4.txt')
 plot_co_data('output5.txt')
+plot_co_data('li-ion_output.txt')
+
+
+plot_co_data('butane_exposure.txt')
+burn <- plot_co_data('burning.txt')
+plot(burn$value, type='l')
 
 plot_co_data <- function(fn) {
   fd <- read.csv(fn, header=FALSE)
   names(fd) <- c('type', 'value')
   plot(fd$value, col=fd$type)
   legend('topleft', legend=names(table(fd$type)), col=c(1:3), pch=1)
+  return(fd)
 }
